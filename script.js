238d656eb7963da569abb0ef6189abf24f480533
Frederik Kristensen <frede4947@email.com>
2024-02-07 14:49:04 +0100

    Trying to make counter work

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-document.getElementById("number1").innerHTML = "new text"
+
+let number1 = 0;
+
+function increaseNumberFunction(number1) {
+    number1 + 1;
+    document.getElementById("nStick").innerHTML = number1;
+    Console.log(number1);       
+    
+}
