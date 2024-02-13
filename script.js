db072e9a7de8922318364b7a4f6fef8fbcde6306
Frederik Kristensen <frede4947@email.com>
2024-02-13 15:45:28 +0100

    Counter working, upgrades and random funtion implemented

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 
-let number1 = 0;
+// Upgrades
+var basket = false
 
-function increaseNumberFunction(number1) {
-    number1 + 1;
-    document.getElementById("nStick").innerHTML = number1;
-    Console.log(number1);       
+// Random function
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+// Gather Functions
+function gatherSticks(vSticks) {
+    var element = document.getElementById('nSticks');
+    var vSticks = element.innerHTML;
+
+    // Increases stick value
+    ++vSticks;
+    if (basket == true) {
+        console.log('yo');
+    }
+
+    console.log('Sticks: ' + vSticks);
+    document.getElementById('nSticks').innerHTML = vSticks;
+    
+}
+
+function gatherStone(vStones) {
+    var element = document.getElementById('nStones');
+    var vStones = element.innerHTML;
+
+    // Increases stone value
+    ++vStones;
+
+    console.log('Stones: ' + vStones);
+    document.getElementById('nStones').innerHTML = vStones;
     
 }
